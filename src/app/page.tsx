0002a31058ad24f8f3c8a2f31486d7ef84c1a5a6
billfromannarbor <bill.heitzeg@gmail.com>
2024-02-08 10:07:22 -0500

    Added in public artifacts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,86 @@
+//'use client'
+
 import Image from "next/image";
+//import 'codemirror/lib/codemirror.css'
+import { useEffect } from 'react'
+//const codeMirrorRef = useRef()
+
+
+
+// useEffect(() => {
+//     require('codemirror/mode/markdown/markdown')
+//     const CodeMirror = require('codemirror')
+//     const instance = CodeMirror.fromTextArea(codeMirrorRef.current, {
+//         lineNumbers: false,
+//         lineWrapping: true,
+//         mode: "text/x-markdown"
+//     })
+// }, [])
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+			<p className="text-3xl font-bold underline">
+			Welcome to CodeQuest!
+			</p>
+			<Image src="/cute-blue-dragon.png" alt="cute blue dragon" width={100} height={100}/>
+				<p className="">
+					To Enter this website, you’ll need to go through the main gate.
+				</p>
+				<p className="">
+					The main gate is locked, but as a developer, you have access to the object maingatelock, which has a method you need: insertKey.
+				</p>
+				<p className="">
+					To open the gate, insert a string with the value “Blue” into the main gate lock.
+				</p>
+				<p className="">
+					Good luck on your Code adventure!
+				</p>
+				
+				<video id="frontgate-animation" src="/OpenGardenGate.mp4" >
+				
+				</video>
+				<section>
+        			<textarea></textarea>
+    			</section>
+				<button className="" type="button" id="btn-run-code">Run Code</button>
+				<div id="javascript-errors" className="alert alert-success" role="alert"></div>
     </main>
   );
 }
+
+
+
+/*
+<div id="welcome" class="container-fluid">
+			<p class="codequest-plaintext lead">
+			Welcome to CodeQuest!
+			</p>
+			<div>
+				<img id="welcome-image" src="img/cute-blue-dragon.png" alt="Welcome to CodeQuest" width="10%" height="10%">
+				<p class="codequest-plaintext lead">
+					To Enter this website, you’ll need to go through the main gate.
+				</p>
+				<p class="codequest-plaintext lead">
+					The main gate is locked, but as a developer, you have access to the object maingatelock, which has a method you need: insertKey.
+				</p>
+				<p class="codequest-plaintext lead">
+					To open the gate, insert a string with the value “Blue” into the main gate lock.
+				</p>
+				<p class="codequest-plaintext lead">
+					Good luck on your Code adventure!
+				</p>
+				
+			</div>
+			<video id="frontgate-animation">
+				<source src="img/OpenGardenGate.mp4" type="video/mp4">
+			</video>
+			<div>
+				<textarea id="scripttoexecute" rows="5" cols="30">
+				
+				</textarea>
+				<button class="btn btn-default" type="button" id="btn-run-code">Run Code</button>
+				<div id="javascript-errors" class="alert alert-success" role="alert"></div>
+			</div>
+			*/
